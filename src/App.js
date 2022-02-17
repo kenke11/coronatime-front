4b5860a2e323ea5bdbe7d7554d5ee66787f78ec5
kenke11 <kenkebashvili2@gmail.com>
2022-02-17 16:40:20 +0400

    feat: use redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import Login from './components/auth/login/Login';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Registration from './components/auth/registration/Registration';
 import Auth from './components/auth/Auth';
 import Dashboard from './components/dashboard/Dashboard';
 import Worldwide from './components/dashboard/worldwide/Worldwide';
 
 function App() {
-  const isLoggedIn = false;
+  const user = useSelector((state) => state.user);
 
   return (
     <>
       <Routes>
         <Route
           path='/'
-          element={<Navigate to={isLoggedIn ? '/dashboard' : '/login'} />}
+          element={<Navigate to={user.isLoggedIn ? '/dashboard' : '/login'} />}
         />
         <Route
           path='/'
-          element={!isLoggedIn ? <Auth /> : <Navigate to={'/dashboard'} />}
+          element={!user.isLoggedIn ? <Auth /> : <Navigate to={'/dashboard'} />}
         >
           <Route path='login' element={<Login />} />
           <Route path='registration' element={<Registration />} />
@@ -25,7 +26,7 @@ function App() {
 
         <Route
           path='/dashboard'
-          element={isLoggedIn ? <Dashboard /> : <Navigate to={'/'} />}
+          element={user.isLoggedIn ? <Dashboard /> : <Navigate to={'/'} />}
         >
           <Route index element={<Worldwide />} />
         </Route>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,15 +10,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './i18next';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Suspense fallback={<div />}>
-      <App />
-    </Suspense>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Suspense fallback={<div />}>
+        <App />
+      </Suspense>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
