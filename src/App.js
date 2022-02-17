b10c1beabcebec4db6d4668a03a71be3a4542007
kenke11 <kenkebashvili2@gmail.com>
2022-02-17 13:24:51 +0400

    chore: install eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Login from './components/auth/login/Login';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="text-red-600">
+    <div className='text-red-600'>
       hello coronatime
+      <Routes>
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </div>
   );
 }
