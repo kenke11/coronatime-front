644ee23858733cd517c0003143352775e71e12c3
kenke11 <kenkebashvili2@gmail.com>
2022-03-05 17:05:48 +0400

    fix: remove margin

--- src/components/dashboard/header/Header.js
+++ src/components/dashboard/header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <>
       <header>
         <div className='relative bg-white border-b-2 border-gray-100'>
-          <div className='px-6 px-4 md:px-28'>
+          <div className='px-6 md:px-28'>
             <div className='flex justify-between items-center py-6'>
               <div className='flex justify-start lg:w-0 lg:flex-1'>
                 <HeaderLogo />
