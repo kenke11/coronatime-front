63dcd033a6f941efaf1bf894c3f7088e440c1bc5
kenke11 <kenkebashvili2@gmail.com>
2022-03-10 17:29:45 +0400

    refactor: use tailvind in 404 page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%': { left: '-100px' },
+          '25%': { transform: 'rotate(30deg)', top: '50%' },
+          '50%': { transform: 'rotate(45deg)', top: '55%' },
+          '75%': { transform: 'rotate(30deg)', top: '60%' },
+          '100%': { transform: 'rotate(45deg)', left: '100%' },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
